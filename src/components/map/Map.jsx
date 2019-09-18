43bc8afa6cbd137f1f4e9bc52d987366286d9242
Ps <ps.world.is.here@gmail.com>
2019-09-18 12:29:38 +0530

    fixed map loading issue button appearing earlier than map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,14 +1,49 @@
 import React from "react"
 
 import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api"
-import { Typography } from "@material-ui/core"
 
+import { makeStyles } from "@material-ui/core/styles"
+import { Typography } from "@material-ui/core"
+import TextField from "@material-ui/core/TextField"
+import { Button } from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    margin: theme.spacing(4),
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+}))
 const apikey = "AIzaSyAUGzly5dIhIJubgTw1aHoUOlrf9TtQi1I"
 
 const Map = props => {
+  const classes = useStyles()
+
+  const [name, setName] = React.useState()
+  const [isLoaded, setIsLoaded] = React.useState(false)
   const [mapRef, setMapRef] = React.useState(null)
   const [center, setCenter] = React.useState(props.center)
-  const { markerPosition, setMarkerPosition } = props
+  const [markerPosition, setMarkerPosition] = React.useState(null)
+
+  if (markerPosition) {
+    var savedLocation = {
+      name: name,
+      lat: markerPosition.lat,
+      lng: markerPosition.lng,
+    }
+  }
+  const encSavedLocation = JSON.stringify(savedLocation)
 
   function handleClick(lat, lng) {
     const latLng = {
@@ -26,7 +61,7 @@ const Map = props => {
           height: "80vh",
           width: "100%",
         }}
-        // onLoad={map => setMapRef(map)}
+        onLoad={() => setIsLoaded(true)}
         zoom={15}
         center={center}
         onClick={e => handleClick(e.latLng.lat(), e.latLng.lng())}
@@ -36,6 +71,33 @@ const Map = props => {
           position={markerPosition}
         />
       </GoogleMap>
+      {isLoaded && (
+        <form className={classes.container}>
+          <TextField
+            onChange={e => setName(e.target.value)}
+            required
+            id="standard-full-width"
+            label="Name"
+            style={{ margin: 8 }}
+            // onFocus={true}
+            placeholder="Name this location"
+            // fullWidth
+            margin="normal"
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <Button
+            onClick={() => localStorage.setItem("lla", btoa(encSavedLocation))}
+            style={{ margin: 8 }}
+            color="secondary"
+            variant="contained"
+          >
+            Save Location
+          </Button>
+        </form>
+      )}
     </LoadScript>
   )
 }
--- src/pages/set-location/manual.jsx
+++ src/pages/set-location/manual.jsx
@@ -1,43 +1,9 @@
 import React, { useState } from "react"
 import Layout from "../../components/layout"
 import Map from "../../components/map/Map"
-import { makeStyles } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
-import TextField from "@material-ui/core/TextField"
-import { Button } from "@material-ui/core"
-
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: "flex",
-    flexWrap: "wrap",
-    margin: theme.spacing(4),
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200,
-  },
-  dense: {
-    marginTop: 19,
-  },
-  menu: {
-    width: 200,
-  },
-}))
 
 const ManualLocation = () => {
-  const classes = useStyles()
-  const [name, setName] = useState()
-  const [markerPosition, setMarkerPosition] = React.useState(null)
-  if (markerPosition) {
-    var savedLocation = {
-      name: name,
-      lat: markerPosition.lat,
-      lng: markerPosition.lng,
-    }
-  }
-  const encSavedLocation = JSON.stringify(savedLocation)
-
   return (
     <Layout>
       <br></br>
@@ -54,37 +20,7 @@ const ManualLocation = () => {
       >
         Set Location
       </button> */}
-      <Map
-        markerPosition={markerPosition}
-        setMarkerPosition={setMarkerPosition}
-        center={{ lat: 31.818543, lng: 76.936076 }}
-      ></Map>
-
-      <form className={classes.container}>
-        <TextField
-          onChange={e => setName(e.target.value)}
-          required
-          id="standard-full-width"
-          label="Name"
-          style={{ margin: 8 }}
-          // onFocus={true}
-          placeholder="Name this location"
-          // fullWidth
-          margin="normal"
-          variant="outlined"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <Button
-          onClick={() => localStorage.setItem("lla", btoa(encSavedLocation))}
-          style={{ margin: 8 }}
-          color="secondary"
-          variant="contained"
-        >
-          Save Location
-        </Button>
-      </form>
+      <Map center={{ lat: 31.818543, lng: 76.936076 }}></Map>
     </Layout>
   )
 }
