c56275f6e3ac792b7daa40400718ad47f1a4b90b
Ps <ps.world.is.here@gmail.com>
2019-11-25 16:18:16 +0530

    Square border logo and https to http

--- src/components/apollo/client.js
+++ src/components/apollo/client.js
@@ -2,7 +2,8 @@ import ApolloClient from "apollo-boost"
 import { fetch } from "isomorphic-fetch"
 
 const client = new ApolloClient({
-  uri: "https://raspaai-env.xyjg76auqq.ap-south-1.elasticbeanstalk.com/graphql",
+  uri: "http://raspaai-env.xyjg76auqq.ap-south-1.elasticbeanstalk.com/graphql",
+// uri: "http://localhost:8000/graphql",
   credentials: "include",
   fetch,
 })
